9a5362a82103a1ee1673fd3a1fe9d8a2e8af72d7
Vitaliy Turkevich <vetalturkevich@gmail.com>
2018-11-18 02:46:50 -0500

    Completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
+function closed() {
+    document.getElementById("cookie").style.display="none";
+}
+
 function hide($el) {
     $el.closest('[id]').style.display="none";
+    document.getElementById("blackout").style.display="none";
 }
 function popUps($id) {
+    document.getElementById("blackout").style.display="block";
     document.getElementById($id).style.display="block";
+
 }
